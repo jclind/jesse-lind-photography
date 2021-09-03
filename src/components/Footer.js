4013e7551ab926eb9b198be9f10eed70e2c0a7a3
Jesse Lind <Jesseclind@gmail.com>
2021-09-03 13:40:33 -0400

    add footer to home page

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <span className="copyright">
+          &#169; {new Date().getFullYear()} Jesse Lind. All rights reserved.
+          Built with <a href="https://www.gatsbyjs.com/">Gatsby</a>
+        </span>
+        <span className="liscense">
+          All photos owned by Jesse Lind, <Link to="/contact">contact me</Link>{" "}
+          for photo usage and pricing.
+        </span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
